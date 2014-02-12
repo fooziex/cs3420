a949603034a00f219eb0b64f80ef09769d4daf6b
dooz <thofsommer@gmail.com>
2014-02-12 13:55:12 -0600

    Update board.py
    fixed board size code

--- board.py
+++ board.py
@@ -1,8 +1,8 @@
 class Board():
     """Store the 5x5 board data"""
     Bx=5 #x size
-    By=5 #y size
+    By=5 #y size              # <- these should probably be constants somewhere
     Cn=4 #number to connect
     marks=list()
     for i in range(Bx):
-        marks.append([0]*5)
+        marks.append([0]*By)
