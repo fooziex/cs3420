5c17987981de60e3193e9a3ac73c7077f5febabc
dooz <thofsommer@gmail.com>
2014-02-25 23:52:41 -0600

    fix tabs

--- ai.py
+++ ai.py
@@ -1,8 +1,8 @@
 class AI:
-	def make_random_move(board,player_name='CPU'):
-		while True: #faking a do while
-			xpos = random.randint(5)
-			ypos = random.randint(5)
-			if board.marks[xpos][ypos] == None: #this, with the break below, is like the while()
-				board.marks[xpos][ypos] = player_name
-				break
+    def make_random_move(board,player_name='CPU'):
+        while True: #faking a do while
+            xpos = random.randint(5)
+            ypos = random.randint(5)
+            if board.marks[xpos][ypos] == None: #this, with the break below, is like the while()
+                board.marks[xpos][ypos] = player_name
+                break
