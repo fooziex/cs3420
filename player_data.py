ed75ed42bd9cb31b6086209455896b0775abbab0
dooz <thofosmmer@gmail.com>
2014-04-16 08:41:13 -0500

    load_player function works

--- player_data.py
+++ player_data.py
@@ -1,6 +1,25 @@
 import sqlite3 as sl
+from constants import PLAYER_DATABASE
 
 class Player_Data():
-    #name
-    #wins
-    #losses
+    name = None
+    wins = None
+    losses = None
+
+    def load_player(self,playername):
+        conn = sl.connect(PLAYER_DATABASE)
+        c = conn.cursor()
+        playername_sql = (playername,)
+        c.execute('SELECT * FROM players WHERE name=?', playername_sql)
+        if c.arraysize == 1:
+            player = c.fetchone()
+            self.name = player[1]
+            self.wins = player[2]
+            self.losses = player[3]
+        else:
+            raise Exception #should be more specific
+        return player
+
+
+    def create_player(self,playername):
+        pass
