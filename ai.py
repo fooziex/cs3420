125dd145012e36adf8eb5d0671d0a1ceeef8758a
dooz <thofsommer@gmail.com>
2014-04-27 14:41:41 -0500

    add check_impending_win()

--- ai.py
+++ ai.py
@@ -1,11 +1,26 @@
+from constants import WINNING_POSITIONS
 import random
 
 class AI():
-    def make_random_move(self,board,player_name='CPU'):
-        while True: #faking a do while
-            xpos = random.randint(0,4)
-            ypos = random.randint(0,4)
-            if board.marks[xpos][ypos] == None: #this, with the returns below, is like the while()
+    def make_random_move(self, board, player_name):
+        while board.check_winning_positions() == (False, False):
+            xpos = random.randint(0, 4)
+            ypos = random.randint(0, 4)
+            if board.marks[xpos][ypos] is None:  # this, with the returns below, is like the while()
                 board.marks[xpos][ypos] = player_name
                 return board
-        #There's a bug in make_random_move somewhere. It's probably to do with how Python deals with classes; I get an error when I try to pass the board object to this function. I'll fix it unless someone else figures it out.
+        print(board.check_winning_positions()[0]+' wins with '+str(board.check_winning_positions()[1]))
+
+    def check_impending_win(self, board, player_name):
+        for winning_move in WINNING_POSITIONS:
+            for i in range(4):
+                almost_moves = list(winning_move)
+                check_move = almost_moves[i]
+                almost_moves.remove(check_move)
+                count = 0
+                for place in almost_moves:
+                    if board.marks[place[0]][place[1]] == player_name :
+                        count = count + 1
+                if count == 3 :
+                    if board.marks[check_move[0]][check_move[1]] is None:
+                        return check_move
