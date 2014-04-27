f647d0d16e55a11d2d6dcbc0f06191c7a7261d65
dooz <thofsommer@gmail.com>
2014-04-27 13:37:33 -0500

    player_data mostly complete
    using SQLAlchemy. Has functions: list_players, load_player, create_player.

--- player_data.py
+++ player_data.py
@@ -1,25 +1,73 @@
-import sqlite3 as sl
 from constants import PLAYER_DATABASE
 
-class Player_Data():
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, Unicode
+
+engine = create_engine('sqlite:///' + PLAYER_DATABASE)
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
+
+class Player(Base):
+    __tablename__ = 'players'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, unique=True, nullable=False)
+    wins = Column(Integer, default=0)
+    losses = Column(Integer, default=0)
+
+    def __repr__(self):
+        return "<Player(id='%s', name='%s', wins='%s', losses='%s')>" % (self.id, self.name, self.wins, self.losses)
+
+
+class PlayerData():
+    id = None
     name = None
     wins = None
     losses = None
 
-    def load_player(self,playername):
-        conn = sl.connect(PLAYER_DATABASE)
-        c = conn.cursor()
-        playername_sql = (playername,)
-        c.execute('SELECT * FROM players WHERE name=?', playername_sql)
-        if c.arraysize == 1:
-            player = c.fetchone()
-            self.name = player[1]
-            self.wins = player[2]
-            self.losses = player[3]
+    def list_players(self):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        players = session.query(Players).all()
+        player_names = set()
+        for player in players:
+            player_names.add(player.name)
+        session.close()
+        return player_names
+
+    def load_player(self, playername):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        player = session.query(Players).filter_by(name=playername)
+        if player.count() == 1:
+            player_data = player.first()
+            self.id = player_data.id
+            self.name = player_data.name
+            self.wins = player_data.wins
+            self.losses = player_data.losses
         else:
-            raise Exception #should be more specific
-        return player
+            session.close()
+            raise Exception  #should be more specific
+        session.close()
+        return player_data
 
+    def create_player(self, playername):
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    def create_player(self,playername):
-        pass
+        if session.query(Player).filter_by(name=playername).count() == 0:
+            new_player = Player(name=playername)
+            session.add(new_player)
+            self.id = new_player.id
+            self.name = new_player.name
+            self.wins = new_player.wins
+            self.losses = new_player.losses
+            session.commit()
+            session.close()
+        else:
+            raise Exception  #should be more specific
+
+    def __repr__(self):
+        return "<Player(id='%s', name='%s', wins='%s', losses='%s')>" % (self.id, self.name, self.wins, self.losses)
