a062d2855d4343d1604f48727edf964fb8a75745
dooz <thofsommer@gmail.com>
2014-04-30 08:25:10 -0500

    Update menu.py
    final commit

--- menu.py
+++ menu.py
@@ -1,37 +1,53 @@
 from player_data import PlayerData
 
 from guidata.dataset.datatypes import DataSet, BeginGroup, EndGroup
-from guidata.dataset.dataitems import ButtonItem, ChoiceItem, TextItem
+from guidata.dataset.dataitems import ButtonItem, ChoiceItem, TextItem, StringItem
 
 
-player = PlayerData()
-players = list(player.list_players())
-players.sort()
+
 
 
 class MainMenu():
     import guidata
     _app = guidata.qapplication()
 
-    def warn_duplicate_players(self, player_one=None, player_two=None):
+    player = PlayerData()
+    players = list(player.list_players())+['New Player']
+    players.sort()
+    players_no_guest = list(player.list_players().difference(set(['Guest'])))
+    players_no_guest.sort()
+
+
+    def warn_duplicate_players(self):
         class WarnDuplicatePlayers(DataSet):
             """C4o5x5"""
-            warning = TextItem('Warning', 'Please choose two different player names.')
+            warning = StringItem('Warning', 'Please choose two different player names.')
         wdp=WarnDuplicatePlayers()
         wdp.view()
 
+    def warn_new_player(self):
+        class WarnNewPlayer(DataSet):
+            """C4o5x5"""
+            warning = StringItem('Warning', 'Please choose a unique player name.')
+        wnp=WarnNewPlayer()
+        wnp.view()
+
+    def warn_no_humans(self):
+        class WarnNoHumans(DataSet):
+            """C4o5x5"""
+            warning = StringItem('Warning', 'Please choose at least one human player.')
+        wnh=WarnNoHumans()
+        wnh.view()
+
     def init_menu(self):
-        exit_menuitem = lambda a, b, c, d: exit()
-        history_menuitem = lambda a, b, c, d: self.show_player_history(player.load_player(players[a.player_one]),
-                                                                       player.load_player(players[a.player_two]))
+        history_menuitem = lambda mainmenu,b,c,d: self.show_player_history(self.player.load_player(self.players_no_guest[mainmenu.player_history]))
 
         class MenuItems(DataSet):
             """C4o5x5"""
-            register = ButtonItem('Register', exit_menuitem)
-            history = ButtonItem('History', history_menuitem)
-            player_one = ChoiceItem("Player 1", players)
-            player_two = ChoiceItem("Player 2", players)
-            first = ChoiceItem("Who Goes First?", ("Player 1", "Player 2"))
+            player_history = ChoiceItem("Play record for:", self.players_no_guest)
+            history = ButtonItem('Display', history_menuitem).set_pos(col=1)
+            player_one = ChoiceItem("Player 1", self.players)
+            player_two = ChoiceItem("Player 2", self.players)
 
         return MenuItems
 
@@ -42,30 +58,50 @@ class MainMenu():
         player_one = None
         player_two = None
         exit_value = None
-        while player_one == player_two and exit_value != 0:
+        while exit_value != 1:
             exit_value = menu.edit()
             player_one = menu.player_one
             player_two = menu.player_two
-            if player_one == player_two and exit_value != 0:
+            if self.players[player_one] == self.players[player_two] and exit_value != 0 \
+                    and self.players[player_one] != 'Guest' and self.players[player_two] != 'Guest' and self.players[player_one] != 'New Player' and self.players[player_two] != 'New Player':
                 self.warn_duplicate_players()
-        return menu.player_one, menu.player_two
+                continue
+            elif exit_value == 0:
+                raise Exception
+            else:
+                break
+        return [self.players[menu.player_one], self.players[menu.player_two]]
 
-    def init_player_history(self, player_one, player_two):
+
+    def init_player_history(self, this_player):
         class PlayerHistoryItems(DataSet):
             """C4o5x5"""
-            player_one_group_begin = BeginGroup(player_one.name)
-            player_one_wins = TextItem('Wins', player_one.wins)
-            player_one_losses = TextItem('Losses', player_one.losses)
-            player_one_group_end = EndGroup(player_one.name)
-
-            player_two_group_begin = BeginGroup(player_two.name)
-            player_two_wins = TextItem('Wins', player_two.wins)
-            player_two_losses = TextItem('Losses', player_two.losses)
-            player_two_group_end = EndGroup(player_two.name)
+            player_one_group_begin = BeginGroup(this_player.name)
+            player_one_wins = StringItem('Wins', this_player.wins)
+            player_one_losses = StringItem('Losses', this_player.losses)
+            player_one_group_end = EndGroup(this_player.name)
 
         return PlayerHistoryItems
 
-    def show_player_history(self, a, b):
-        PlayerHistoryItems = self.init_player_history(a, b)
+    def show_player_history(self, this_player):
+        PlayerHistoryItems = self.init_player_history(this_player)
         history = PlayerHistoryItems()
         history.view()
+
+    def init_register_player(self, player_number=None):
+        class PlayerRegisterItems(DataSet):
+            """c4o5x5"""
+            if player_number is not None:
+                player_name = StringItem('Player '+str(player_number)+' Name')
+            else:
+                player_name = StringItem('Name')
+        return PlayerRegisterItems
+
+    def show_register_player(self,player_number=None):
+        PlayerRegisterItems = self.init_register_player(player_number)
+        accept = 0
+        register = PlayerRegisterItems()
+        while accept == 0:
+            accept = register.edit()
+        new_player=register.player_name
+        return new_player
