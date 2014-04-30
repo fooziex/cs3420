9731ebd1ba70a1938369dc108e9837d9c896688d
dooz <thofsommer@gmail.com>
2014-04-30 08:46:51 -0500

    final commit to diplay_board.py
    final commit

--- display_board.py
+++ display_board.py
@@ -1,80 +1,157 @@
-import guidata
-_app = guidata.qapplication()
+from constants import HOME_DIRECTORY
 
-import guidata.dataset.datatypes as dt
-import guidata.dataset.dataitems as di
-from guidata.dataset.dataitems import ChoiceItem, ColorItem
+import sys, os, random
 
-class DisplayBoard(dt.DataSet):
-    """c4o5x5
-    The following text is the DataSet 'comment': <br>Plain text or
-    <b>rich text<sup>2</sup></b> are both supported,
-    as well as special characters (α, β, γ, δ, ...)
-    """ #leaving this in as an example
-    
-    #ideally we'd have an array of 5x5 but the UI toolkit won't render arrays like it does variables, as far as I can tell
-    #worst case we're writing a "move from UI to backend class" function
-    A1 = ColorItem("A1", default="black")
-    A2 = ColorItem("A2", default="black").set_pos(col=1)
-    A3 = ColorItem("A3", default="black").set_pos(col=2)
-    A4 = ColorItem("A4", default="black").set_pos(col=3)
-    A5 = ColorItem("A5", default="black").set_pos(col=4)
-    B1 = ColorItem("B1", default="black")
-    B2 = ColorItem("B2", default="black").set_pos(col=1)
-    B3 = ColorItem("B3", default="black").set_pos(col=2)
-    B4 = ColorItem("B4", default="black").set_pos(col=3)
-    B5 = ColorItem("B5", default="black").set_pos(col=4)
-    C1 = ColorItem("C1", default="black")
-    C2 = ColorItem("C2", default="black").set_pos(col=1)
-    C3 = ColorItem("C3", default="black").set_pos(col=2)
-    C4 = ColorItem("C4", default="black").set_pos(col=3)
-    C5 = ColorItem("C5", default="black").set_pos(col=4)
-    D1 = ColorItem("D1", default="black")
-    D2 = ColorItem("D2", default="black").set_pos(col=1)
-    D3 = ColorItem("D3", default="black").set_pos(col=2)
-    D4 = ColorItem("D4", default="black").set_pos(col=3)
-    D5 = ColorItem("D5", default="black").set_pos(col=4)
-    E1 = ColorItem("E1", default="black")
-    E2 = ColorItem("E2", default="black").set_pos(col=1)
-    E3 = ColorItem("E3", default="black").set_pos(col=2)
-    E4 = ColorItem("E4", default="black").set_pos(col=3)
-    E5 = ColorItem("E5", default="black").set_pos(col=4)
-    
-class ChooseMove(dt.DataSet):
-    """c4o5x5
-    Player N, select your move:
-    """ #need to make N change with player number
-    move_set=[((0,0), 'A1'),
-             ((0,1), "A2"),
-             ((0,2), "A3"),
-             ((0,3), "A4"),
-             ((0,4), "A5"),
-             ((1,0), 'B1'),
-             ((1,1), "B2"),
-             ((1,2), "B3"),
-             ((1,3), "B4"),
-             ((1,4), "B5"),
-             ((2,0), 'C1'),
-             ((2,1), "C2"),
-             ((2,2), "C3"),
-             ((2,3), "C4"),
-             ((2,4), "C5"),
-             ((3,0), 'D1'),
-             ((3,1), "D2"),
-             ((3,2), "D3"),
-             ((3,3), "D4"),
-             ((3,4), "D5"),
-             ((4,0), 'E1'),
-             ((4,1), "E2"),
-             ((4,2), "E3"),
-             ((4,3), "E4"),
-             ((4,4), "E5")
-             ]
-    Move = ChoiceItem("Single choice 1",
-                        move_set)
-    
-board_results = DisplayBoard() #this stuff is just for demo purposes
-board_results.view()           #this too    
+os.environ["PYSDL2_DLL_PATH"] = HOME_DIRECTORY
 
-new_move = ChooseMove()        #ditto
-new_move.edit()                #"
+try:
+    from sdl2 import *
+    import sdl2.ext as sdl2ext
+except ImportError:
+    import traceback
+    traceback.print_exc()
+    sys.exit(1)
+
+from sdl2.ext import Resources
+
+
+
+class GameBoard():
+
+    RESOURCES = Resources(HOME_DIRECTORY, "graphics")
+    factory = sdl2ext.SpriteFactory(sdl2ext.SOFTWARE)
+
+    def game_board(self, board, player_one, player_two, player_types, ai_difficulty=None):
+
+        from player_data import PlayerData
+
+        pd=PlayerData()
+
+        if 'AI' in player_types:
+            from ai import AI
+            ai=AI()
+            if 'AI: Easy' in [player_one,player_two]:
+                ai.difficulty=0
+            elif 'AI: Medium' in [player_one,player_two]:
+                ai.difficulty=1
+            elif 'AI: Hard' in [player_one,player_two]:
+                ai.difficulty=2
+
+        sdl2ext.init()
+
+        window = sdl2ext.Window("c4o5x5", size=(320, 420))
+
+        window.show()
+
+        factory = sdl2ext.SpriteFactory(sdl2ext.SOFTWARE)
+
+        spriterenderer = factory.create_sprite_render_system(window)
+
+        moves = 0
+        running = True
+        updated_p1 = False
+        updated_p2 = False
+
+        while running:
+            if moves % 2 == 0:
+                this_player = player_one
+                that_player = player_two
+                if player_types[0]=='AI':
+                    mode='AI'
+                    player_graphic="ai.bmp"
+                else:
+                    mode='Human'
+                    player_graphic = "player_one.bmp"
+            if moves % 2 == 1:
+                this_player = player_two
+                that_player = player_one
+                if player_types[1]=='AI':
+                    mode='AI'
+                    player_graphic="ai.bmp"
+                else:
+                    mode='Human'
+                    player_graphic = "player_two.bmp"
+            for x in range(5):
+                for y in range(5):
+                    if board.marks[x][y] is None:
+                        spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile.bmp")),x*64,y*64)
+                    if board.marks[x][y] == player_one:
+                        spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile_o.bmp")),x*64,y*64)
+                    if board.marks[x][y] == player_two:
+                        spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile_x.bmp")),x*64,y*64)
+            if board.check_winning_positions() != (False,False):
+                winning_player = board.check_winning_positions()[0]
+                if winning_player == player_one:
+                    winning_graphic = 'player_one_wins.bmp'
+                    if player_one != 'Guest' and not updated_p1:
+                        player=pd.load_player(player_one)
+                        pd.update_player(player_one,player.wins+1,player.losses)
+                        updated_p1 = True
+                    if player_two != 'Guest' and not updated_p2:
+                        player=pd.load_player(player_two)
+                        pd.update_player(player_two,player.wins,player.losses+1)
+                        updated_p2=True
+                elif winning_player == player_two:
+                    winning_graphic = 'player_two_wins.bmp'
+                    if player_one != 'Guest' and not updated_p2:
+                        player=pd.load_player(player_two)
+                        pd.update_player(player_two,player.wins+1,player.losses)
+                        updated_p2=True
+                    if player_two != 'Guest' and not updated_p1:
+                        player=pd.load_player(player_one)
+                        pd.update_player(player_one,player.wins,player.losses+1)
+                        updated_p1=True
+                spriterenderer.render(factory.from_image(self.RESOURCES.get_path(winning_graphic)),0,(x+1)*64)
+                events = sdl2ext.get_events()
+                for event in events:
+                    if event.type == SDL_MOUSEBUTTONDOWN:
+                        running = False
+                        SDL_Delay(1000)
+                        break
+            elif moves == 25:
+                winning_graphic = "tie_game.bmp"
+                spriterenderer.render(factory.from_image(self.RESOURCES.get_path(winning_graphic)),0,(x+1)*64)
+                events = sdl2ext.get_events()
+                for event in events:
+                    if event.type == SDL_MOUSEBUTTONDOWN:
+                        running = False
+                        break
+            elif mode == 'Human':
+                spriterenderer.render(factory.from_image(self.RESOURCES.get_path(player_graphic)),0,(x+1)*64)
+                events = sdl2ext.get_events()
+                for event in events:
+                    if event.type == SDL_QUIT:
+                        running = False
+                        break
+                    elif event.type == SDL_MOUSEBUTTONDOWN:
+                        x_pos = int(event.button.x/64)
+                        y_pos = int(event.button.y/64)
+                        if x_pos <= 4 and y_pos <= 4 and board.marks[x_pos][y_pos] is None:
+                            board.marks[x_pos][y_pos]=this_player
+                            moves = moves + 1
+            elif mode=='AI':
+                move = ai.make_move(board,this_player,that_player)
+                moves = moves + 1
+                board.update_position(move[0],move[1],this_player)
+                for x in range(5):
+                    for y in range(5):
+                        if board.marks[x][y] is None:
+                            spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile.bmp")),x*64,y*64)
+                        if board.marks[x][y] == player_one:
+                            spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile_o.bmp")),x*64,y*64)
+                        if board.marks[x][y] == player_two:
+                            spriterenderer.render(factory.from_image(self.RESOURCES.get_path("tile_x.bmp")),x*64,y*64)
+                spriterenderer.render(factory.from_image(self.RESOURCES.get_path(player_graphic)),0,(x+1)*64)
+                SDL_Delay(500)
+                events = sdl2ext.get_events()
+                for event in events:
+                    if event.type == SDL_QUIT:
+                        running = False
+                        break
+                    else:
+                        pass
+            else:
+                raise Exception
+            SDL_Delay(10)
+
+        sdl2ext.quit()
